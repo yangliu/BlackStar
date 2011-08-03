85570328cfb3eca1b5d1e53eccb238d95dbf8e3b
Yang Liu <i@yangliu.name>
2011-08-03 22:26:49 +0800

    Admin can see hidden files in recent page

--- black_star/sys/listpage.py
+++ black_star/sys/listpage.py
@@ -4,6 +4,7 @@
 from black_star import app, render_template, session, request, abort
 from black_star.sys.db import db_session
 from black_star.sys.models import UFile
+from black_star.sys.funcs import is_admin_login
 from black_star import config
 import logging
 import math
@@ -11,14 +12,18 @@ import math
 @app.route('/recent/')
 @app.route('/recent/<int:page_cur>')
 def recent_files(page_cur = None):
+  is_admin = is_admin_login()
   if page_cur is None: page_cur = 1
-  file_num = UFile.query.filter(UFile.homeshow == True).count()
+  file_query = UFile.query
+  if not is_admin:
+    file_query = file_query.filter(UFile.homeshow == True)
+  file_num = file_query.count()
   page_num = int(math.ceil(float(file_num)/float(config.ITEMS_PER_PAGE)))
   if page_num <= 0: abort(404)
   all_pages = range(1, page_num+1)
   if page_cur not in all_pages: abort(404)
   
-  files = UFile.query.filter(UFile.homeshow == True).order_by(UFile.created.desc()).limit(config.ITEMS_PER_PAGE).offset((page_cur-1)*config.ITEMS_PER_PAGE).all()
+  files = file_query.order_by(UFile.created.desc()).limit(config.ITEMS_PER_PAGE).offset((page_cur-1)*config.ITEMS_PER_PAGE).all()
   
   page_title = "Recent Files (Page %d/%d)" % (page_cur, page_num)
   return render_template('list.html', title=page_title, all_pages=all_pages, page_cur=page_cur, files=files)
