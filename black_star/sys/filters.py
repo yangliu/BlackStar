d59a07e27724525c7360813bc22f1990526290b3
Yang Liu <i@yangliu.name>
2011-08-06 21:46:12 +0800

    fix a bug in list page when displaying file size

--- black_star/sys/filters.py
+++ black_star/sys/filters.py
@@ -12,4 +12,25 @@ def file_ext(s):
   if '.' not in s:
     return 'NONE'
   return s.rsplit('.', 1)[1].upper()
+
+@app.template_filter('bs_filesize')
+def bs_filesize(s):
+  try:
+    size = int(s)
+  except:
+    return s
+  
+  if size > 1000*1000*1000*1000:
+    return "%.1f %s" % (float(size)/float(1000*1000*1000*1000),'TB')
+  elif size > 1000*1000*1000:
+    return "%.1f %s" % (float(size)/float(1000*1000*1000),'GB')
+  elif size > 1000*1000:
+    return "%.1f %s" % (float(size)/float(1000*1000),'MB')
+  elif size > 1000:
+    return "%.1f %s" % (float(size)/float(1000),'KB')
+  elif size > 1:
+    return "%d %s" % size,'Bytes'
+  else:
+    return 'N/A'        
+  
   
